72561db108e541ef9c3191a4e3dd96413c5c8963
Weslly Honorato <weslly.honorato@gmail.com>
2011-10-21 15:44:12 -0200

    Update sublimecp.py

--- sublimecp.py
+++ sublimecp.py
@@ -25,7 +25,7 @@ class ColorPickCommand(sublime_plugin.TextCommand):
                 start_color = "#"+selected
                 
         # get new color from picker
-        args = [path.join(sublime.packages_path(), 'ColorPicker', 'lib', 'colorchooser.py')]
+        args = [path.join(sublime.packages_path(), 'SublimeColorPicker', 'lib', 'colorchooser.py')]
         if start_color:
             args.append(start_color)
 
