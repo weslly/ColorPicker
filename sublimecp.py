afbe8c29362a5f4617dbd66e39bd85f150e628d3
Weslly Honorato <weslly.honorato@gmail.com>
2011-12-12 17:06:26 -0200

    chmod +x for linux

--- sublimecp.py
+++ sublimecp.py
@@ -42,7 +42,12 @@ class ColorPickCommand(sublime_plugin.TextCommand):
                 args.append(start_color_osx)
 
         else:
-            args = [os.path.join(sublime.packages_path(), 'ColorPicker', 'lib', 'linux_colorpicker.py')]
+            location = [os.path.join(sublime.packages_path(), 'ColorPicker', 'lib', 'linux_colorpicker.py')]
+            args = [location]
+
+            if not os.access(location, os.X_OK):
+                os.chmod(location, 0755)
+            
             if start_color:
                 args.append(start_color)
 
