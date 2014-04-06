1320c899b0c15ab763b6c5aa6d0298f1a75429cc
pravic <ehysta@gmail.com>
2014-04-06 14:26:14 +0400

    removed ChooseColor

--- sublimecp.py
+++ sublimecp.py
@@ -10,50 +10,6 @@ sublime_version = 2
 if not sublime.version() or int(sublime.version()) > 3000:
     sublime_version = 3
 
-if sublime.platform() == 'windows':
-    import ctypes
-    from ctypes import c_int32, c_uint32, c_void_p, c_wchar_p, pointer, POINTER
-
-    class CHOOSECOLOR(ctypes.Structure):
-         _fields_ = [('lStructSize', c_uint32),
-                     ('hwndOwner', c_void_p),
-                     ('hInstance', c_void_p),
-                     ('rgbResult', c_uint32),
-                     ('lpCustColors',POINTER(c_uint32)),
-                     ('Flags', c_uint32),
-                     ('lCustData', c_void_p),
-                     ('lpfnHook', c_void_p),
-                     ('lpTemplateName', c_wchar_p)]
-
-    class POINT(ctypes.Structure):
-         _fields_ = [('x', c_int32),
-                     ('y', c_int32)]
-
-    CustomColorArray = c_uint32 * 16
-    CC_SOLIDCOLOR = 0x80
-    CC_RGBINIT = 0x01
-    CC_FULLOPEN = 0x02
-
-    ChooseColorW = ctypes.windll.Comdlg32.ChooseColorW
-    ChooseColorW.argtypes = [POINTER(CHOOSECOLOR)]
-    ChooseColorW.restype = c_int32
-
-    GetDC = ctypes.windll.User32.GetDC
-    GetDC.argtypes = [c_void_p]
-    GetDC.restype = c_void_p
-
-    ReleaseDC = ctypes.windll.User32.ReleaseDC
-    ReleaseDC.argtypes = [c_void_p, c_void_p] #hwnd, hdc
-    ReleaseDC.restype = c_int32
-
-    GetCursorPos = ctypes.windll.User32.GetCursorPos
-    GetCursorPos.argtypes = [POINTER(POINT)] # POINT
-    GetCursorPos.restype = c_int32
-
-    GetPixel = ctypes.windll.Gdi32.GetPixel
-    GetPixel.argtypes = [c_void_p, c_int32, c_int32] # hdc, x, y
-    GetPixel.restype = c_uint32 # colorref
-
 
 class ColorPickCommand(sublime_plugin.TextCommand):
     # SVG Colors spec: http://www.w3.org/TR/css3-color/#svg-color
@@ -231,33 +187,6 @@ class ColorPickCommand(sublime_plugin.TextCommand):
 
         if sublime.platform() == 'windows':
 
-            s = sublime.load_settings("ColorPicker.sublime-settings")
-            custom_colors = s.get("custom_colors", ['0']*16)
-
-            if len(custom_colors) < 16:
-                custom_colors = ['0']*16
-                s.set('custom_colors', custom_colors)
-
-            cc = CHOOSECOLOR()
-            ctypes.memset(ctypes.byref(cc), 0, ctypes.sizeof(cc))
-            cc.lStructSize = ctypes.sizeof(cc)
-
-            if sublime_version == 2:
-                cc.hwndOwner = self.view.window().hwnd()
-            else:
-                # Temporary fix for Sublime Text 3 - For some reason the hwnd crashes it
-                # Of course, clicking out of the colour picker and into Sublime will make
-                # Sublime not respond, but as soon as you exit the colour picker it's ok
-                cc.hwndOwner = None
-
-            cc.Flags = CC_SOLIDCOLOR | CC_FULLOPEN | CC_RGBINIT
-            cc.rgbResult = c_uint32(start_color_win) if not paste and start_color_win else self.__get_pixel()
-            cc.lpCustColors = self.__to_custom_color_array(custom_colors)
-
-            if ChooseColorW(ctypes.byref(cc)):
-                color = self.__bgr_to_hexstr(cc.rgbResult)
-            else:
-                color = None
 
 
         elif sublime.platform() == 'osx':
