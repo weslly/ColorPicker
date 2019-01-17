be7a3456df822215cce58f6ca5fd8341096258c6
pwnbit <pwnbit@gmail.com>
2019-01-17 10:57:05 +0900

    Update: Return value removes when cancel.
    New 'win_colorpicker.exe' is return word 'CANCEL' when cancel.
    And the modified code has been changed to work only on windows.

--- sublimecp.py
+++ sublimecp.py
@@ -324,8 +324,12 @@ class ColorPicker(object):
             ):
                 color = color.decode('utf-8')
                 
-        # New win_Colorpicker.exe has return hex color code and alpha code. (ex: #FF0000FF = Red color + transparency 100%)
-        color = color.replace('#','')[:6]
+                # New win_Colorpicker.exe has return hex color code and alpha code. (ex: #FF0000FF = Red color + transparency 100%)
+                # Only applied to 'win_colorpicker.exe'.
+                if color == "CANCEL":    # When canceled.
+                    color = ""
+                else:
+                    color = color.replace('#','')[:6]
         
         return color
 
